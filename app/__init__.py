eb021132dfa6431769f6140aecf3fa992d8a1b90
JoachimTislov <joachim.tislov@gmail.com>
2023-10-11 18:12:32 +0200

    lol

--- app/__init__.py
+++ app/__init__.py
@@ -8,25 +8,28 @@ from flask import Flask
 from app.config import Config
 from app.database import SQLite3
 
-# from flask_login import LoginManager
+from flask import Flask, request, redirect
+
+#from flask_login import LoginManager
 # from flask_bcrypt import Bcrypt
 # from flask_wtf.csrf import CSRFProtect
 
 # Instantiate and configure the app
 app = Flask(__name__)
+
 app.config.from_object(Config)
 
 # Instantiate the sqlite database extension
 sqlite = SQLite3(app, schema="schema.sql")
 
 # TODO: Handle login management better, maybe with flask_login?
-# login = LoginManager(app)
+#login = LoginManager(app)
 
 # TODO: The passwords are stored in plaintext, this is not secure at all. I should probably use bcrypt or something
 # bcrypt = Bcrypt(app)
 
 # TODO: The CSRF protection is not working, I should probably fix that
-# csrf = CSRFProtect(app)
+#csrf = CSRFProtect(app)
 
 # Create the instance and upload folder if they do not exist
 with app.app_context():
